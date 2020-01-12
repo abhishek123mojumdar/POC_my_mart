f6e574e08d17d1ee2d3d97cae11885746600816f
Dibya Ranjan Bhoi <dibyabhoidibya@gmail.com>
2020-01-12 16:22:51 +0530

    Added Social Login Buttons

--- components/Login.js
+++ components/Login.js
@@ -1,57 +1,56 @@
-import React, {Component, useState} from 'react';
-import {StyleSheet, Text, Button, TextInput, View} from 'react-native';
+import React, { Component, useState } from "react";
+import { StyleSheet, Text, Button, TextInput, View } from "react-native";
+import { SocialIcon } from "react-native-elements";
 
-const Login=() => {
+const Login = () => {
+  const [value, changeValue] = useState("Loggedout");
+  const [btnvalue, btnchangeValue] = useState("Login");
 
-   const [value , changeValue]  = useState ('Loggedout');
-  const [btnvalue , btnchangeValue]  = useState ('Login');
+  const changeText = () => {
+    changeValue(value == "Loggedout" ? "LoggedIn" : "Loggedout");
+    btnchangeValue(btnvalue == "Login" ? "Logout" : "Login");
+  };
 
-  const changeText = ()=>{changeValue(value == 'Loggedout' ? 'LoggedIn' : 'Loggedout');
-  btnchangeValue(btnvalue == 'Login' ? 'Logout' : 'Login') }
-
-  
-        return(
+  return (
     <View style={styles.container}>
       <Text>{value}</Text>
-      <TextInput
-          style={{height: 40}}
-          placeholder="Username"
-        />
-         <TextInput
-          style={{height: 40}}
-          placeholder="passowrd"
-        />
-      <Button title={btnvalue} onPress={changeText}/>
-    </View>
+      <TextInput style={{ height: 40 }} placeholder="Username" />
+      <TextInput style={{ height: 40 }} placeholder="passowrd" />
+      <Button title={btnvalue} onPress={changeText} />
 
-        );
-       
-    }
+      <View style={styles.buttonContainer}>
+        <SocialIcon title="Sign In With Facebook" button type="facebook" />
+
+        <SocialIcon title="Sign In With Google" button type="google" />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
-  header:{
-      backgroundColor: 'pink',
-      padding:50
+  header: {
+    backgroundColor: "pink",
+    padding: 50
   },
-  boldText:{
-      fontWeight: 'bold'
+  boldText: {
+    fontWeight: "bold"
   },
 
-  buttonContainer:{
-      marginTop:20 
+  buttonContainer: {
+    marginTop: 20
   },
-  input:{
-      borderWidth:1,
-      borderColor: '#777',
-      padding:8,
-      margin:10,
-      width:200
+  input: {
+    borderWidth: 1,
+    borderColor: "#777",
+    padding: 8,
+    margin: 10,
+    width: 200
   }
 });
 
