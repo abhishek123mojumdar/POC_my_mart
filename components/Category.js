851eec2d70ed093033a41408954d27c620094dec
Dibya Ranjan Bhoi <dibyabhoidibya@gmail.com>
2020-01-12 15:49:58 +0530

    Added correct Folder Structure
    Created Individual Components and fixed folder structure

---
+++ components/Category.js
@@ -0,0 +1,27 @@
+import React, {Component, useState} from 'react';
+import {StyleSheet, Text, Button, TextInput, View} from 'react-native';
+
+const Category=() => {
+
+  
+
+  
+        return(
+    <View style={styles.container}>
+      
+    </View>
+
+        );
+       
+    }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
+
+export default Category;
